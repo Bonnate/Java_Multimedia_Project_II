c35c5a486f027ac4e961737741a3cdada4f85bb3
Bonnate <vldkdhtudrjs@gmail.com>
2022-09-06 11:53:06 +0900

    Initial commit

---
+++ 0908/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static Scanner Scanner;
+	
+	public static void main(String[] args) 
+	{
+		Scanner = new Scanner(System.in);
+		
+		new ProductManager();
+	}
+}
