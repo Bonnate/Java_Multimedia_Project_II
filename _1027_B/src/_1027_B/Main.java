2e77469c1868c470a677cf287c9903069e559afd
Bonnate <vldkdhtudrjs@gmail.com>
2022-10-25 11:08:35 +0900

    1027 연습문제 2번

---
+++ _1027_B/src/_1027_B/Main.java
@@ -0,0 +1,45 @@
+package _1027_B;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashSet<Person>hSet=new HashSet<Person>();
+		hSet.add(new Person("Lee",24));
+		hSet.add(new Person("Hong",29));
+		hSet.add(new Person("Choi",21));
+		hSet.add(new Person("Hong",28));
+		printCollection(hSet.iterator());
+		
+		System.out.println();
+		
+		deleteCollection(hSet,"Hong");
+		printCollection(hSet.iterator());
+	}
+
+	public static void printCollection(Iterator<Person> itr) {
+		while (itr.hasNext())
+			itr.next().ShowData();
+	}
+	
+	public static void deleteCollection(HashSet<Person> hSet, String name) 
+	{
+		hSet.remove(new Person(name));
+	}
+	
+	public static void deleteCollectionSearch(HashSet<Person> hSet, String name)
+	{
+		Iterator<Person> itr = hSet.iterator();
+		
+		while (itr.hasNext())
+		{
+			if(itr.next().GetName().equals(name))
+			{
+				itr.remove();
+			}
+		}
+	}
+}
---
+++ _1027_B/src/_1027_B/Person.java
@@ -0,0 +1,45 @@
+package _1027_B;
+
+public class Person {
+	private String mName;
+	private int mAge;
+
+	public Person(String name, int age) {
+		this.mName = name;
+		this.mAge = age;
+	}
+	
+	public Person(String name) {
+		this.mName = name;
+	}
+	
+	public String GetName()
+	{
+		return this.mName;
+	}
+
+	public void ShowData() {
+		System.out.println(this.mName + " " + this.mAge);
+	}
+
+	@Override
+	public int hashCode() {
+		return this.mName.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
+
+		if (!(obj instanceof Person))
+			return false;
+		
+		Person other = (Person) obj;
+		if (!this.mName.equals(other.mName))
+			return false;
+		
+		
+		return true;
+	}
+}
